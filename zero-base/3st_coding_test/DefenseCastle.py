9085c5c857c26a3bb2297f76268930e03388c9ff
sgyeong97 <sgyeong97@gmail.com>
2022-02-24 20:25:27 +0900

    No.5 TestCase 추가

--- zero-base/3st_coding_test/DefenseCastle.py
+++ zero-base/3st_coding_test/DefenseCastle.py
@@ -1,4 +1,4 @@
-#5¹ø ¹®Á¦
+#No.5
 def solution(distance, time):
     enemy = []
     defance = 0
@@ -13,3 +13,16 @@ def solution(distance, time):
         if defance < attack:
             return -1
     return defance-attack
+
+if __name__ == '__main__':
+    # test_case = [[1,2,8],[1,1,2],1]
+    test_case = [[2,2,3,6],[1,1,1,2],-1]
+    # test_case = [[1,1],[1,1],-1]
+    # test_case = [[3,3],[1,1],1]
+    # test_case = [[1,5,7],[1,2,3],-1]
+    # test_case = [[3,3,3],[1,1,1],0]
+    result = solution(test_case[0], test_case[1])
+    if result == test_case[2]:
+        print(f"Success [{result}]")
+    else:
+        print(f"Fail [{result}]")
